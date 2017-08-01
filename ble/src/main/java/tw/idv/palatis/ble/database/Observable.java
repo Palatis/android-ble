9ca043865be1393c0c9fb19d7522bc6b3a83f3c9
Victor Tseng <palatis@gmail.com>
2017-08-02 06:49:13 +0800

    fix dead-lock

--- ble/src/main/java/tw/idv/palatis/ble/database/Observable.java
+++ ble/src/main/java/tw/idv/palatis/ble/database/Observable.java
@@ -2,30 +2,42 @@ package tw.idv.palatis.ble.database;
 
 import android.support.annotation.NonNull;
 
+import java.util.ArrayList;
+
 public abstract class Observable<ObserverT> extends android.database.Observable<ObserverT> {
     @Override
-    public synchronized void registerObserver(@NonNull ObserverT observer) {
+    public void registerObserver(@NonNull ObserverT observer) {
         try {
-            super.registerObserver(observer);
+            synchronized (mObservers) {
+                super.registerObserver(observer);
+            }
         } catch (IllegalStateException ignored) {
         }
     }
 
     @Override
-    public synchronized void unregisterObserver(@NonNull ObserverT observer) {
+    public void unregisterObserver(@NonNull ObserverT observer) {
         try {
-            super.unregisterObserver(observer);
+            synchronized (mObservers) {
+                super.unregisterObserver(observer);
+            }
         } catch (IllegalStateException ignored) {
         }
     }
 
-    public synchronized int numObservers() {
-        return mObservers.size();
+    public int numObservers() {
+        synchronized (mObservers) {
+            return mObservers.size();
+        }
     }
 
-    protected synchronized void notifyChange(@NonNull Notifier<ObserverT> notifier) {
-        for (int i = mObservers.size() - 1; i >= 0; --i)
-            notifier.notifyChange(mObservers.get(i));
+    protected void notifyChange(@NonNull Notifier<ObserverT> notifier) {
+        final ArrayList<ObserverT> observers;
+        synchronized (mObservers) {
+            observers = new ArrayList<>(mObservers);
+        }
+        for (int i = observers.size() - 1; i >= 0; --i)
+            notifier.notifyChange(observers.get(i));
     }
 
     public interface Notifier<ObserverT> {
